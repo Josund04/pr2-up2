255d11f223885292150696381b1371113e0797b6
Josund04 <josu04001@utb.vaxjo.se>
2022-09-13 11:29:25 +0200

    1an klar

--- pr2 up2/Program.cs
+++ pr2 up2/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+public class AreaOfRectangle
+{
+    public static void Main()
+    {
+        int height = 15;
+        int width = 6;
+        int Area = height * width;
+        Console.WriteLine("Arean av basen" (width) "och höjden " (height) "ger arean " (Area));
+        Console.ReadKey();
+    }
+} 
